9cbc5ff24acdde13ce1e7c0ed89c33ee217e4a8f
deibidsonmesquita <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>
2022-08-11 15:23:30 -0300

    Heroku e swagger jdk 17

--- src/main/java/com/apisports/ApiSportsApplication.java
+++ src/main/java/com/apisports/ApiSportsApplication.java
@@ -1,8 +1,13 @@
 package com.apisports;
 
+import com.apisports.repository.ClubeRepositorio;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ApiSportsApplication {
 
@@ -10,4 +15,17 @@ public class ApiSportsApplication {
         SpringApplication.run(ApiSportsApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner runner(ClubeRepositorio repositorio) {
+        return args -> {
+            System.out.println("Executando api..");
+        };
+    }
+
+/*    @Bean
+    @Primary
+    public PasswordEncoder getPasswordEncoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }*/
 }
